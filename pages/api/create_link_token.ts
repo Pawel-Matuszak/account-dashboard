799b0e76f342ac754a33ee3f00d0c2699ff8deec
pawelmatuszak <pawelxmatuszak@gmail.com>
2023-02-15 20:27:26 +0100

    changes

--- pages/api/create_link_token.ts
+++ pages/api/create_link_token.ts
@@ -3,24 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { Configuration } from 'plaid';
 import { CountryCode, LinkTokenCreateRequest, LinkTokenCreateResponse, PlaidApi, Products } from 'plaid/dist/api';
 import { PlaidEnvironments } from 'plaid/dist/configuration';
-
-const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
-const PLAID_SECRET = process.env.PLAID_SECRET;
-const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
-
-
-const configuration = new Configuration({
-  basePath: PlaidEnvironments[PLAID_ENV],
-  baseOptions: {
-    headers: {
-      'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
-      'PLAID-SECRET': PLAID_SECRET,
-      'Plaid-Version': '2020-09-14',
-    },
-  },
-});
-
-const client = new PlaidApi(configuration);
+import client from './client';
 
 export default async function handler(
   req: NextApiRequest,
--- pages/api/set_access_token.ts
+++ pages/api/set_access_token.ts
@@ -1,10 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { LinkTokenCreateResponse } from 'plaid';
+import client from './client';
 
 type Data = {
-  name: string
+  public_token_exchange: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -12,7 +14,19 @@ export default function handler(
     if (req.method != 'POST') {
       res.status(405).end();
     }; 
-    // req.body.public_token
+    const publicToken = req.body.public_token;
+  try {
+    const response = await client.itemPublicTokenExchange({
+      public_token: publicToken,
+    });
+    // These values should be saved to a persistent database and
+    // associated with the currently signed-in user
+    const accessToken = response.data.access_token;
+    const itemID = response.data.item_id;
+    res.json({ public_token_exchange: 'complete' });
+  } catch (error) {
+    // handle error
+  }
     
   } catch (error) {
     res.status(500)
